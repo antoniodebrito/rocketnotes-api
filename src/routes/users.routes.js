d4106c17e0ccc1b28510b027d36e0ba180f0a850
antoniodebrito <antonio11brito@gmail.com>
2023-03-11 18:42:45 -0300

    :sparkles: feat:save img on the database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,21 @@
 const { Router } = require('express');
 const usersRoutes = Router();
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
+const upload = multer(uploadConfig.MULTER);
 
 const UsersController = require("../controller/UsersController");
+const UserAvatarController = require("../controller/UserAvatarController");
+
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 
 module.exports = usersRoutes;
